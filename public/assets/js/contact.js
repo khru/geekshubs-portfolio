d2d0c79c2d3cc158222822f861b54dad7997dba6
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2020-04-10 11:28:13 +0200

    GeeksHubs-frontend: Redesign som features and added security features

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,14 +1,32 @@
+/*
+ The main reason to use this script is that the mailto
+*/
+
 "use strict";
 
 document.querySelector("#contact-form").addEventListener("submit", sendEmail)
 
 function sendEmail(event) {
 	event.preventDefault();
-	const firstName = document.querySelector("#first-name").value;
-	const lastName = document.querySelector("#last-name").value;
+	const firstName = document.querySelector("#first-name") ? document.querySelector("#first-name").value.trim() : null;
+	const lastName = document.querySelector("#last-name") ? document.querySelector("#last-name").value.trim() : null;
+	const message = document.querySelector("#message") ? document.querySelector("#message").value : null;
+
+
+	if (null === firstName || null === lastName || null === message) {
+		alert("You should not be messing with the site 😤");
+		location.reload();
+	}
+
+	if (firstName.length < 1 || lastName.length < 1 || message.length < 1 ) {
+		alert("All the fields are required 😜");
+		return;
+	}
+
 	const fullName = `${firstName} ${lastName}`;
-	const message = document.querySelector("#message").value;
-	const mailto = encodeURI(`mailto:evrtrabajo@gmail.com?subject=${fullName}&body=${message}`);
-	location.href=mailto;
+	const email = "evrtrabajo@gmail.com";
+
+	const mailto = encodeURI(`mailto:${email}?subject=${fullName}&body=${message}`);
+	window.open(mailto, '_blank');
 }
 
